945b77145d1c0ca96b219c9b17939ba57a808059
bill r <jy@ozzis.us>
2017-07-18 16:41:09 -0400

    udemy 7-55

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,48 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-var About = (props) => <h3>About</h3>;
+var About = (props) => {
+	return (
+		<div>
+			<h1 className="text-centered">Examples</h1>
+			<p>A small sample app built using the following tools:</p>
+			<ol>
+				<li>
+					<Link to="https://facebook.github.io/react/">React</Link>
+				</li>
+				<li>
+					<Link to="http://foundation.zurb.com/">Foundation</Link>
+				</li>
+				<li>
+					<Link to="https://nodejs.org/en/">Node</Link>
+				</li>
+				<li>
+					<Link to="http://expressjs.com/">Express</Link>
+				</li>
+				<li>
+					<Link to="https://webpack.github.io/">Webpack</Link>
+				</li>
+				<li>
+					<Link to="https://www.npmjs.com/package/axios">Axios</Link>
+				</li>
+				<li>
+					<Link to="https://dashboard.heroku.com/apps">Heroku</Link>
+				</li>
+				<li>
+					<Link to="https://github.com/bill209/reactWx">GitHub repo for this app</Link>
+				</li>
+				<li>
+					<Link to="http://openweathermap.org">Thanks to OpenWeatherMap!</Link>
+				</li>
+
+				{/*
+				<li>
+					<Link to=""></Link>
+				</li>
+*/}
+			</ol>
+		</div>
+	)
+}
 
 module.exports = About;
